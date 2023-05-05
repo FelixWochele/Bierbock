bde729dec8c97af98474007d1652bd7c9cabd3c1
SevaPypenko <pypenko.vv1402s@gmail.com>
2023-05-05 21:12:48 +0200

    we are so dumb

--- app/src/main/java/com/bierbock/BackendFolder/BackendRequest.java
+++ app/src/main/java/com/bierbock/BackendFolder/BackendRequest.java
@@ -48,6 +48,8 @@ public abstract class BackendRequest {
         }
 
         url = urlBuilder.toString();
+
+        url = url.replace("@", "%40");
     }
 
     //Set Task delegate callback here:
@@ -65,69 +67,5 @@ public abstract class BackendRequest {
         backend.execute(url, authenticationToken, body);
     }
 
-
-    /*
-    public LinkedHashMap<String, Object> parseJsonObject(JSONObject object) throws JSONException {
-
-        LinkedHashMap<String, Object> resultMap = new LinkedHashMap<>();
-
-        try {
-            // Check if the "result" key contains a JSONObject or a JSONArray
-            Object result = object.get("result");
-
-            if (result instanceof JSONObject) {
-                // Process the "result" JSONObject
-                JSONObject resultObject = (JSONObject) result;
-                resultMap = jsonToLinkedHashMap(resultObject);
-            } else if (result instanceof JSONArray) {
-                // Process the "result" JSONArray
-                JSONArray resultArray = (JSONArray) result;
-                resultMap = jsonArrayToLinkedHashMap(resultArray);
-            } else {
-                // Handle unexpected "result" value types
-                // ...
-            }
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        return resultMap;
-    }
-
-    private static LinkedHashMap<String, Object> jsonToLinkedHashMap(JSONObject jsonObject) throws JSONException {
-        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
-
-        for (Iterator<String> iterator = jsonObject.keys(); iterator.hasNext();) {
-            String key = iterator.next();
-            Object value = jsonObject.get(key);
-            map.put(key, value);
-        }
-
-        return map;
-    }
-
-    private static LinkedHashMap<String, Object> jsonArrayToLinkedHashMap(JSONArray jsonArray) throws JSONException {
-        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
-
-        for (int i = 0; i < jsonArray.length(); i++) {
-            JSONObject jsonObject = jsonArray.getJSONObject(i);
-
-            for (Iterator<String> iterator = jsonObject.keys(); iterator.hasNext();) {
-                String key = iterator.next();
-                Object value = jsonObject.get(key);
-
-                if (!map.containsKey(key)) {
-                    ArrayList<Object> list = new ArrayList<>();
-                    map.put(key, list);
-                }
-                ((ArrayList<Object>) map.get(key)).add(value);
-            }
-        }
-
-        return map;
-    }
-
-*/
 
 }
